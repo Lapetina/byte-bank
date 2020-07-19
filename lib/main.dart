61d37f0979132d77e4516acd264896256b9f1f98
Lapetina <victor.lrs95@gmail.com>
2020-07-19 00:46:54 -0300

    refactor: create ListaTransfers, Transfer

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
+        body: ListaTransfers(),
         appBar: AppBar(
           title: Text('Transferências'),
         ),
@@ -13,4 +16,44 @@ void main() {
       ),
     )
   );
+}
+
+class ListaTransfers extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        ItensTransfers(Transfer(100.0, 1001)),
+        ItensTransfers(Transfer(200.0, 1001)),
+        ItensTransfers(Transfer(300.0, 3003)),
+      ],
+    );
+  }
+}
+
+class ItensTransfers extends StatelessWidget {
+
+  final Transfer _transfer;
+
+  ItensTransfers(this._transfer);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        leading: Icon(Icons.monetization_on),
+        title: Text(_transfer.value.toString()),
+        subtitle: Text(_transfer.accountNumber.toString()),
+      ),
+    );
+  }
+
+}
+
+class Transfer {
+  final double value;
+  final int accountNumber;
+
+  Transfer(this.value, this.accountNumber);
+
 }
