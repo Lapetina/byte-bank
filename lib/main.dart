1649bff7b2b5f34de3db531b9c3e065b9cb5527c
Lapetina <victor.lrs95@gmail.com>
2020-07-18 23:49:40 -0300

    style: Add MaterialDesign

--- lib/main.dart
+++ lib/main.dart
@@ -2,22 +2,15 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    Column(
-      children: <Widget>[
-        Text('Deliver features faster', textDirection: TextDirection.ltr),
-        Text('Craft beautiful UIs', textDirection: TextDirection.ltr),
-        Expanded(
-          child: FittedBox(
-          fit: BoxFit.contain, // otherwise the logo will be tiny
-          child: const FlutterLogo(),
-          ),
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Transferências'),
         ),
-        Column(
-          children: <Widget>[
-            Text('Segunda coluna', textDirection: TextDirection.ltr),
-          ],
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
         ),
-      ],
-    ),
+      ),
+    )
   );
 }
