43765fa5e6a96284e38aaa265abc8ebe37ff986e
Lapetina <victor.lrs95@gmail.com>
2020-07-22 01:25:40 -0300

    feat: Add settings in buttons

--- lib/main.dart
+++ lib/main.dart
@@ -19,11 +19,65 @@ class BytebankApp extends StatelessWidget {
 
 
 class FormTransfer extends StatelessWidget {
+  final TextEditingController _controlAccountNumber = TextEditingController();
+  final TextEditingController _controlValue = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Criando Transf'),),
-        body: Text('teste')
+      appBar: AppBar(
+        title: Text('Criando Transferência'),
+      ),
+      body: Column(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+              child: TextField(
+                style: TextStyle(
+                  fontSize: 16.0,
+                ),
+                decoration: InputDecoration(
+                  labelText: 'Número da conta',
+                  hintText: '0000'
+                ),
+                keyboardType: TextInputType.number,
+                controller: _controlAccountNumber,
+              ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+              child: TextField(
+                style: TextStyle(
+                  fontSize: 16.0,
+                ),
+                decoration: InputDecoration(
+                  labelText: 'Valor',
+                  hintText: '0.00',
+                  icon: Icon(Icons.monetization_on),
+                ),
+                keyboardType: TextInputType.number,
+                controller: _controlValue,
+              ),
+          ),
+          RaisedButton(
+            child: Text('Confirmar'),
+            onPressed: () {
+              debugPrint('cliclou no confirmar');
+              final int accountNumber = int.tryParse(
+                  _controlAccountNumber.text);
+              final double valueNumber = double.tryParse(_controlValue.text);
+              if (accountNumber != null && valueNumber != null) {
+                final transfer = Transfer(accountNumber, valueNumber);
+                print(accountNumber);
+                print(valueNumber);
+                debugPrint('$transfer');
+                final snackBar = SnackBar(content: Text('$transfer'));
+                Scaffold.of(context).showSnackBar(snackBar);
+              }
+            },
+          ),
+        ],
+      ),
     );
   }
 }
@@ -39,9 +93,9 @@ class ListTransfers extends StatelessWidget{
       ),
       body: Column(
         children: [
-          ItensTransfers(Transfer(100.0, 1001)),
-          ItensTransfers(Transfer(200.0, 1001)),
-          ItensTransfers(Transfer(300.0, 3003)),
+          ItensTransfers(Transfer(1001, 100.0)),
+          ItensTransfers(Transfer(1001, 200.0)),
+          ItensTransfers(Transfer(3003, 300.0)),
         ],
       ),
       floatingActionButton: FloatingActionButton(
@@ -69,8 +123,13 @@ class ItensTransfers extends StatelessWidget {
 }
 
 class Transfer {
-  final double value;
   final int accountNumber;
+  final double value;
 
-  Transfer(this.value, this.accountNumber);
+  @override
+  String toString() {
+    return 'Transfer{accountNumber: $accountNumber, value: $value}';
+  }
+
+  Transfer(this.accountNumber, this.value);
 }
